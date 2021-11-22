299d10cba2dc1cc28458fb33a8b91a57649896c4
AyumiM5 <mmmiray.555@gmail.com>
2021-11-22 08:39:52 +0000

    [Fix]rayout

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,14 +2,12 @@ class Spot < ApplicationRecord
   
   belongs_to :note
   
-  # 存在性のバリデーション
   validates :note_id, presence: true
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :body, presence: true, length: { minimum: 2, maximum: 100 }
   validates :latitude, presence: true
   validates :longitude, presence: true
 
-  # バリデーションの前に送信されたaddressの値によってジオコーディング(緯度経度の算出)を行う
   geocoded_by :address
   before_validation :geocode
   
