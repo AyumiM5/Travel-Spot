20754e7b1c6b1fc37cd5d8663c1c073e648cd014
AyumiM5 <mmmiray.555@gmail.com>
2021-11-27 14:12:25 +0000

    [Fix]tag

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,6 @@
 class Note < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
+  
   belongs_to :user
   has_many :spots, dependent: :destroy
   has_many :note_comments, dependent: :destroy
